10b4a95f80f25a54c79e121a705e0daf08ba986f
Tim Greller <timlg07@gmail.com>
2020-06-25 22:50:18 +0200

    EnfNum: Created class and test-case.

---
+++ Sudoku-Solver/src/de/tim_greller/sudoku/model/EnforcedNumber.java
@@ -0,0 +1,15 @@
+package de.tim_greller.sudoku.model;
+
+public class EnforcedNumber implements Saturator {
+    
+    /** 
+     * {@inheritDoc}
+     * <p>This strategy (...)</p>
+     */
+    @Override
+    public boolean saturate(Board board) throws UnsolvableSudokuException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
