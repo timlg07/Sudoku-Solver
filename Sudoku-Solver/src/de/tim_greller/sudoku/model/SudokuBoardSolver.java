af5585233f77f7b62870a106aaaf58990d577678
Tim Greller <timlg07@gmail.com>
2020-06-25 11:37:18 +0200

    Solver: created class and generated interface methods.

---
+++ Sudoku-Solver/src/de/tim_greller/sudoku/model/SudokuBoardSolver.java
@@ -0,0 +1,37 @@
+package de.tim_greller.sudoku.model;
+
+import java.util.List;
+
+public class SudokuBoardSolver implements SudokuSolver {
+
+    private List<Saturator> saturators;
+    
+    public SudokuBoardSolver() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void addSaturator(Saturator saturator) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Board saturate(Board board) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Board findFirstSolution(Board board) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Board> findAllSolutions(Board board) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
