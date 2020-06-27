62d11f1eed7b616a27cfb40c434d6e1ebe26ac2f
Tim Greller <timlg07@gmail.com>
2020-06-27 21:22:35 +0200

    Anonymized package names.

--- Sudoku-Solver/src/de/tim_greller/sudoku/io/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package provides utility classes for I/O operations on the data model,
- * such as loading a sudoku from a file and executing operations on it.
- */
-package de.tim_greller.sudoku.io;
