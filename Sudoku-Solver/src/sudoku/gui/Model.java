cce00ef3d41670cea1b9ee5501f0165426b4a3a3
Tim Greller <timlg07@gmail.com>
2020-06-30 16:14:40 +0200

    [gui] added basic operations to the model.

--- Sudoku-Solver/src/sudoku/gui/Model.java
+++ Sudoku-Solver/src/sudoku/gui/Model.java
@@ -1,5 +1,56 @@
 package sudoku.gui;
 
+import java.io.File;
+
+import sudoku.io.SudokuFileParser;
+import sudoku.model.Board;
+import sudoku.model.InvalidSudokuException;
+import sudoku.model.Structure;
+import sudoku.model.SudokuBoard;
+
 public class Model extends Observable {
+    
+    private static final int UNSET_CELL = -1;
+    
+    private int[][] uncheckedBoard;
+    private Board intelligentBoard;
+    private int boxRows;
+    private int boxCols;
+    private int numbers;
+    
+    void loadSudokuFromFile(File sudokuFile) throws InvalidSudokuException {
+        intelligentBoard = SudokuFileParser.parseToBoard(sudokuFile);
+    }
+    
+    void updateDisplayData() {
+        notifyObservers();
+    }
+    
+    void updateDisplayData(Board board) {
+        numbers = board.getNumbers();
+        
+        uncheckedBoard = new int[numbers][numbers];
+        for (int boxNr = 0; boxNr < numbers; boxNr++) {
+            for (int cellNr = 0; cellNr < numbers; cellNr++) {
+                int cellValue = board.getCell(Structure.BOX, boxNr, cellNr);
+                uncheckedBoard[boxNr][cellNr] = ((cellValue == Board.UNSET_CELL)
+                                                 ? UNSET_CELL
+                                                 : cellValue);
+            }
+        }
+        
+        updateDisplayData();
+    }
+    
+    private void createIntelligentBoard() throws InvalidSudokuException {
+        intelligentBoard = new SudokuBoard(boxRows, boxCols);
+        
+        for (int boxNr = 0; boxNr < numbers; boxNr++) {
+            for (int cellNr = 0; cellNr < numbers; cellNr++) {
+                intelligentBoard.setCell(Structure.BOX, boxNr, cellNr,
+                        uncheckedBoard[boxNr][cellNr]);
+            }
+        }
+    }
     
 }
