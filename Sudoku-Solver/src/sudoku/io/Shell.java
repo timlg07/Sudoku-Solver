6c3c8212afd93737de914bc7cdb218ca83101b90
Tim Greller <timlg07@gmail.com>
2020-07-09 20:36:29 +0200

    Implemented catch-blocks around the parser.

--- Sudoku-Solver/src/sudoku/io/Shell.java
+++ Sudoku-Solver/src/sudoku/io/Shell.java
@@ -2,8 +2,10 @@ package sudoku.io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -217,15 +219,23 @@ public final class Shell {
         String filename = tokenizedInput[1].replaceAll("^\"|\"$", "");  
         
         File sudokuFile = new File(filename);
+        String notFoundMsg = "The file \"" + sudokuFile.getAbsolutePath() 
+                             + "\" was not found.";
         if (sudokuFile.canRead() && sudokuFile.isFile()) {
             try {
                 currentBoard = SudokuFileParser.parseToBoard(sudokuFile);
             } catch (InvalidSudokuException e) {
                 printError("The file contains an invalid sudoku.");
+            } catch (ParseException e) {
+                printError(e.getMessage());
+            } catch (FileNotFoundException e) {
+                printError(notFoundMsg);
+            } catch (IOException e) {
+                printError("Unable to read the file " 
+                           + sudokuFile.getAbsolutePath());
             }
         } else {
-            printError("The file \"" + sudokuFile.getAbsolutePath() 
-                       + "\" was not found.");
+            printError(notFoundMsg);
         }
     }
     
