9ce7368aec5dd46f0d8f73d833241d9edb4c83a1
Tim Greller <timlg07@gmail.com>
2020-07-18 15:47:11 +0200

    SFrame: Completed JavaDoc.
    + Some code fixes after checking JavaSwings internal code.

--- Sudoku-Solver/src/sudoku/gui/SudokuFrame.java
+++ Sudoku-Solver/src/sudoku/gui/SudokuFrame.java
@@ -1,6 +1,5 @@
 package sudoku.gui;
 
-import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -12,13 +11,9 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
-import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
-import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -31,6 +26,7 @@ import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import sudoku.gui.model.DisplayData;
@@ -40,28 +36,73 @@ import sudoku.solver.Structure;
 import sudoku.util.Observable;
 import sudoku.util.Observer;
 
+/**
+ * The custom JFrame that is the main window of the graphical user interface.
+ * It contains a {@link SudokuMenuBar} and, if a file was loaded, an editable
+ * sudoku consisting of {@link SudokuCell}s.
+ */
 public class SudokuFrame extends JFrame implements Observer {
     
     private static final long serialVersionUID = 1L;
+    
+    /**
+     * The preferred size of the initial content pane containing only the info 
+     * text.
+     */
     private static final Dimension PREF_SIZE = new Dimension(400, 300);
+    
+    /**
+     * The minimum size the window can have while it still will be displayed as
+     * a proper window and show the menu bar.
+     */
     private static final Dimension MIN_SIZE = new Dimension(240, 150);
+    
+    /**
+     * The border around each box of the sudoku.
+     */
     private static final Border BOX_BORDER 
             = BorderFactory.createBevelBorder(BevelBorder.LOWERED);
     
-    private final DisplayData data;
-    private Collection<AbstractButton> operationsOnSudoku = new ArrayList<>();
+    /**
+     * The filter that will only accept sudoku files (with the extension .sud).
+     */
+    private static final FileFilter SUDOKU_FILE_FILTER 
+            = new FileNameExtensionFilter("Sudoku File", "sud");
     
     /**
+     * The data model which stores the displayed sudoku and provides operations
+     * on it.
+     */
+    private final DisplayData data;
+    
+    /**
+     * A collection of menu items that will perform operations on the current 
+     * sudoku if the user performs an action on them. They will be disabled and
+     * enabled depending on the {@link DisplayData#isOperationOnSudokuAllowed()}
+     * state of the data model.
+     */
+    private Collection<JMenuItem> operationsOnSudoku = new ArrayList<>();
+    
+    /**
+     * The file chooser that should be used to for selecting sudoku files.<p>
      * Using one instance of JFileChooser, the last folder will be remembered.
      */
     private final JFileChooser fileChooser = new JFileChooser();
 
+    /**
+     * Constructs and shows a SudokuFrame window that will always display the
+     * current state of the given dataModel by observing it as an 
+     * {@link sudoku.util.Observer}. The controller included in this frame will
+     * perform the operations on the dataModel the user requests.
+     * 
+     * @param dataModel The data model that stores and manages the displayed 
+     *                  data.
+     */
     public SudokuFrame(DisplayData dataModel) {
         data = dataModel;
         data.attachObserver(this);
 
-        fileChooser.setFileFilter(
-                new FileNameExtensionFilter("Sudoku File", "sud"));
+        fileChooser.setFileFilter(SUDOKU_FILE_FILTER);
         
         setJMenuBar(new SudokuMenuBar());
         setEnableStates();
@@ -89,6 +130,12 @@ public class SudokuFrame extends JFrame implements Observer {
         setVisible(true);
     }
     
+    /**
+     * {@inheritDoc}
+     * Updates the view if a new sudoku is loaded. Only updates the enabled 
+     * state of buttons that can perform operations on the current sudoku if the
+     * sudoku-lock was changed.
+     */
     @Override
     public void update(Observable observable, Object argument) {
         assert observable instanceof DisplayData;
@@ -108,6 +155,11 @@ public class SudokuFrame extends JFrame implements Observer {
         }
     }
     
+    /**
+     * Sets the enabled states of all components, which can do operations on the
+     * current sudoku, to the state stored in the data model. This should lock
+     * (or unlock) all operations on the current sudoku.
+     */
     private void setEnableStates() {
         boolean allowed = data.isOperationOnSudokuAllowed();
         
@@ -116,21 +168,28 @@ public class SudokuFrame extends JFrame implements Observer {
         });
     }
 
+    /**
+     * Completely recreates the components used to display the sudoku board.
+     * This is needed when the first sudoku or a new sudoku with different sizes
+     * is loaded.
+     * <p>
+     * This method will replace the current content pane and then resize and 
+     * relocate the frame.
+     */
     private void resetBoardView() {
         int numbers = data.getNumbers();
         int boxRows = data.getBoxRows();
         int boxCols = data.getBoxCols();
         Container content = new Container();
-
-        // The layout manager used to arrange cells in a box.
+        LayoutManager outerLayout = new GridLayout(boxCols, boxRows);
         LayoutManager innerLayout = new GridLayout(boxRows, boxCols);
         
         // Set the layout manager of the container which contains all boxes.
-        content.setLayout(new GridLayout(boxCols, boxRows));
+        content.setLayout(outerLayout);
         
         /*
          * The sudoku is only displayed correctly when the box coordinate system
-         * is used. For other structures the index must be converted.
+         * is used. For other structures the index has to be converted.
          */
         assert DisplayData.STRUCT == Structure.BOX;
         
@@ -145,22 +204,38 @@ public class SudokuFrame extends JFrame implements Observer {
         
         // Update the visible frame and its content.
         setContentPane(content);
-        validate();
-        pack();
+        pack(); // Handles validation of the whole Container as well.
         setLocationRelativeTo(null);
     }
 
+    /**
+     * This class is the custom {@link JMenuBar} of a SudokuFrame and provides
+     * options to: <ul>
+     *     <li> Close the SudokuFrame.
+     *     <li> Load a new sudoku from a sudoku file.
+     *     <li> Undo the last operation on the current sudoku.
+     *     <li> Suggest a correct value.
+     *     <li> Completely solve the current sudoku.
+     */
     private class SudokuMenuBar extends JMenuBar {
         
         private static final long serialVersionUID = 1L;
 
+        /**
+         * Creates a new menu bar which already contains all menu items with
+         * listeners added.
+         */
         SudokuMenuBar() {
             super();
             
             JMenu fileMenu = new JMenu("File");
             JMenu editMenu = new JMenu("Edit");
             JMenu solveMenu = new JMenu("Solve");
-            
+
+            /*
+             * These menus contain only operations on a sudoku, so they can be
+             * locked completely.
+             */
             operationsOnSudoku.add(editMenu);
             operationsOnSudoku.add(solveMenu);
             
@@ -195,7 +270,9 @@ public class SudokuFrame extends JFrame implements Observer {
         }
     }
     
-    
+    /**
+     * This {@link ActionListener} handles the action of opening a sudoku file.
+     */
     private class OpenFileActionListener implements ActionListener {
 
         @Override
